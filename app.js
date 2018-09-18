b05d2a1629ec702e1dd613dc65cb81d62c87f0b6
igrok666 <programmer.urgzk@gmail.com>
2018-09-19 01:13:33 +0500

    version: 1.07 'Добавлена команда для включения отрицательных чисел' 'Добавлена команда установки шага' 'Исправлен баг для ввода числел с клавиатуры' 'В следующей версии будет добавлена команда для анимации и задержка анимации'

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-        $('.numb').number_plugin();
+        $('.numb').number_plugin({
+            negative: false,
+            step: 300,
+            // animate: true,
+            // delay: 10,
+        });
     });
